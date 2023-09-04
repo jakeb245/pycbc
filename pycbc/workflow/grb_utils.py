91b043cc82df05510e9a96da4c315ffd3914fa3a
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-09-04 08:38:41 -0400

    Remove duplicate inj file input

--- pycbc/workflow/grb_utils.py
+++ pycbc/workflow/grb_utils.py
@@ -502,14 +502,12 @@ def make_pygrb_plot(workflow, exec_name, out_dir,
     # IFO option
     if ifo:
         node.add_opt('--ifo', ifo)
-    # Additional input files (passed as File instances)
-    if exec_name in ['pygrb_plot_injs_results', 'pygrb_efficiency']:
-        fm_file = resolve_url_to_file(inj_file)
-        node.add_input_opt('--found-missed-file', fm_file)
     # Output files and final input file (passed as a File instance)
     if exec_name == 'pygrb_efficiency':
         # In this case tags[0] is the offtrial number
         logging.info("Setting up efficiency specific opts")
+        fm_file = resolve_url_to_file(inj_file)
+        node.add_input_opt('--found-missed-file', fm_file)
         onsource_file = configparser_value_to_file(workflow.cp,
                                                    'workflow', 'onsource-file')
         node.add_input_opt('--onsource-file', onsource_file)
